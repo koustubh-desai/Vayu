938a03f86ab1fa7ab689782e30a1c08d5256ae20
koustubh-desai <koustubh-desai@users.noreply.github.com>
2017-03-15 13:58:10 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<head>
+ <meta charset="<?php bloginfo( 'charset' ); ?>">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <!--<link rel="profile" href="http://gmpg.org/xfn/11">
+ <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">-->
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://rawgit.com/koustubh-desai/cricminute/master/mainStyle.css">
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+ <script src="https://rawgit.com/koustubh-desai/cricminute/master/mainScript.js"></script>
+ <!-- I am in head -->
+ <?php wp_head() ?>
+</head>
